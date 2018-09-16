27df9415956d4832b4db3e21fad8b4b0ecc1aa6f
Daniel <dflocked@gmail.com>
2018-09-16 02:01:40 -0400

    Added compose file for bring up infrastructure, started splitting up scrapers into own module

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate reqwest;
-// extern crate scraper;
+extern crate scraper;
+
 use std::env;
 use std::process;
 
@@ -12,9 +13,33 @@ const HELP_STR: &str = "
 
 fn scrape() {
     let mu_releases_url = "https://www.mangaupdates.com/releases.html";
-    let html = reqwest::get(mu_releases_url).unwrap().text().unwrap();
-    println!("Successfully got \n{}", html);
-    // #main_content > div > div:nth-child(4) > table > tbody > tr:nth-child(6) > td.pad > a
+    let mu_response_html = reqwest::get(mu_releases_url).unwrap().text().unwrap();
+
+    let mu_release_html_parsed = scraper::Html::parse_document(mu_response_html.as_str());
+
+    let mu_release_title_selector = scraper::Selector::parse(
+        "#main_content > div > div:nth-child(4) > table > tbody > tr > td.pad > a",
+    ).unwrap();
+    let mu_release_chapter_selector = scraper::Selector::parse(
+        "#main_content > div > div:nth-child(4) > table > tbody > tr > td:nth-child(2)",
+    ).unwrap();
+    let mu_release_group_selector = scraper::Selector::parse(
+        "#main_content > div > div:nth-child(4) > table > tbody > tr > td:nth-child(3) > a",
+    ).unwrap();
+
+    for title in mu_release_html_parsed.select(&mu_release_title_selector) {
+        println!("manga names = {}", title.inner_html());
+        println!("manga url = {}", title.value().attr("href").unwrap());
+    }
+
+    for ch in mu_release_html_parsed.select(&mu_release_chapter_selector) {
+        println!("manga chapter = {}", ch.inner_html());
+    }
+
+    for group in mu_release_html_parsed.select(&mu_release_group_selector) {
+        println!("manga group = {}", group.inner_html());
+        println!("manga group url = {}", group.value().attr("href").unwrap());
+    }
 }
 
 fn main() {
